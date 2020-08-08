d39a812c6f280ad4162b56941dd0cc43d818be84
Wesley Bueno <wesley.bueno.silva.wbs@gmail.com>
2020-08-07 21:01:59 -0300

    V2

---
+++ viajantes/src/test/java/com/programem/viajantes/ViajantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programem.viajantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
